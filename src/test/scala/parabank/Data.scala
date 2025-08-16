6dfeb90d3035685b1c0db85b3d299362d46bd037
emiliogrwoo <emiliogrwoo@gmail.com>
2025-08-16 09:50:40 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,5 +4,9 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "Emigrwoo"
     val password = "Chocorramo1*"
+    val fromAccountId = "16119"
+    val toAccountId = "16230"
+    val amount = "20"
 
 }
+
